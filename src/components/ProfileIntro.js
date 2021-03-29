65c528a9899fee3deca7506afffa53b13864e588
Nguyen Le Ly Bang <bangjdev@gmail.com>
2021-03-29 18:11:09 +0200

    Add profile picture, update styles, use smaller pics

--- src/components/ProfileIntro.js
+++ src/components/ProfileIntro.js
@@ -3,17 +3,22 @@ import '../css/ProfileIntro.css';
 
 const ProfileIntro = () => {
     return (
-        <div id="intro">
-            <span id="intro-title">Full Stackoverflow Developer</span>
-            <div id="intro-body">
-                <h1>Hello,<br/>call me Bang</h1>
-                <span id="intro-headline">
-                    I do fullstack web development and I love playing video games
-                </span>
-                <div id="intro-buttons">
-                    <button>Projects</button>
-                    <button>LinkedIn</button>
+        <div id='profile-intro'>
+            <div id='intro'>
+                <span id='intro-title'>Full Stackoverflow Developer</span>
+                <div id='intro-body'>
+                    <h1>Hello,<br/>call me Bang</h1>
+                    <span id='intro-headline'>
+                        I do fullstack web development and I love playing video games
+                    </span>
+                    <div id='intro-buttons'>
+                        <button>Projects</button>
+                        <button>LinkedIn</button>
+                    </div>
                 </div>
+            </div>
+            <div id="profile-image">
+
             </div>
         </div>
     );
